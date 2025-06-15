1fef96ebe91c929cbaf2ce9cfd43ef35aa8f7642
installer <installer@gmail.com>
2025-06-15 11:52:31 +0000

    user.nix: home-manager migrate

--- hosts/regolith/home.nix
+++ hosts/regolith/home.nix
@@ -1,18 +1,66 @@
-{ pkgs, ... }: # We only need 'pkgs' if we're adding packages, but it's common to include.
+# /home/roger/regolith-nix-starter-config/hosts/regolith/home.nix
+{ pkgs, ... }:
 
 {
   # IMPORTANT: Set your Home Manager state version.
-  # This MUST match what you configured in your flake.nix file
-  # (where you defined home-manager.users."your-username" = { home.stateVersion = "..." })
-  # Assuming you set it to "24.05" previously:
-  home.stateVersion = "24.05";
+  # This MUST match what you configured in your flake.nix (e.g., "24.05")
+  home.stateVersion = "24.05"; # Ensure this is correct for your system/channel
 
-  # Add one simple package to verify Home Manager is working.
-  # 'hello' is a standard GNU program that just prints "Hello, world!".
+  # Add some user-specific packages
   home.packages = with pkgs; [
     hello
+    # If you decide to move lsd and fzf from environment.systemPackages in users.nix,
+    # you would add them here:
+    # lsd
+    # fzf
   ];
 
-  # No other configurations for now. We will add them back gradually.
-  # No programs.zsh, no git config, etc.
+  # --- Zsh Configuration (MIGRATED from users.nix) ---
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    ohMyZsh = {
+      enable = true;
+      plugins = ["git"];
+      theme = "agnoster";
+    };
+
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    promptInit = ''
+      fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
+
+      #pokemon colorscripts like.
+      # Make sure to install krabby package
+      #krabby random --no-mega --no-gmax --no-regional --no-title -s;
+
+      # Set-up icons for files/directories in terminal using lsd
+      alias ls='lsd'
+      alias l='ls -l'
+      alias la='ls -a'
+      alias lla='ls -la'
+      alias lt='ls --tree'
+
+      source <(fzf --zsh);
+
+      HISTFILE=~/.zsh_history;
+      HISTSIZE=10000;
+      SAVEHIST=10000;
+      setopt appendhistory;
+    '';
+  };
+
+  # You can now add other Home Manager specific configurations here,
+  # such as Git user config, other program settings, dotfiles, etc.
+  # For example, to manage your Git user name and email through Home Manager:
+  # programs.git = {
+  #   enable = true;
+  #   userName = "Roger";
+  #   userEmail = "roger@example.com";
+  #   extraConfig = {
+  #     init.defaultBranch = "main";
+  #   };
+  # };
 }
